379d0375f770f889d65bb5ae0c663c4fc32725eb
Josan Behrends <josanbehrends@gmail.com>
2024-01-20 16:23:28 -0300

    Galeria finalizada e corrigida

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Banner from "./componentes/Banner"
 import bannerBackground from './assets/banner.png'
 import Galeria from "./componentes/Galeria"
 
+import fotos from './fotos.json'
+import { useState } from "react"
+import ModalZoom from "./componentes/ModalZoom"
+
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
@@ -13,25 +17,26 @@ const FundoGradiente = styled.div`
 `
 
 const AppContainer = styled.div`
-  width: 1200px;
+  width: 1440px;
   margin: 0 auto;
   max-width: 100%;
 `
 
 const MainContainer = styled.main`
   display: flex;
-  gap: 10%;
+  gap: 24px;
 `
 
 const ConteudoGaleria = styled.section`
-  display:flex;
+  display: flex;
   flex-direction: column;
   flex-grow: 1;
 `
 
-function App() {
-
-
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+  const [fotoSelecionada, setFotoSelecionada] = useState(null)
+  
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -44,10 +49,17 @@ function App() {
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
-            <Galeria />
+            <Galeria 
+              aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+              fotos={fotosDaGaleria}
+            />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
+      <ModalZoom 
+        foto={fotoSelecionada}
+        aoFechar={() => setFotoSelecionada(null)}
+      />
     </FundoGradiente>
   )
 }
